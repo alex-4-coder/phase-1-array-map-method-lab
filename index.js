39b347dd4e910006e6bf1dd483708339d525b2e4
alex kamau <alex.kamau1@student.moringaschool.com>
2025-06-12 21:08:13 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const tutorials = [
-  'what does the this keyword mean?',
+  'What does the this keyword mean?',
   'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
+  'Implementing Blockchain Web API',
   'The Test Driven Development Workflow',
   'What is NaN and how Can we Check for it',
   'What is the difference between stopPropagation and preventDefault?',
@@ -12,5 +12,11 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map(tutorial => {
+    return tutorial 
+    .split(" ")
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ")
+
+  });
+};
